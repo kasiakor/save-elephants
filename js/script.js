7edc8949f099afcf236a0e0e50cb406d335e5481
Kasia Kordys <kasia.kordys@gmail.com>
2017-03-07 23:45:25 +0000

    Change JSON format

--- js/script.js
+++ js/script.js
@@ -1,420 +1,419 @@
+[
 {
-"products": [
-	{
-	"product_id": "0_150",
-	"description": "Silver Elephant Pendant Chain Necklace",
-	"collection": "best_seller",
-	"product_type": "jewellery",
-	"price": "30.00"
-	},
-	{
-	"product_id": "0_151",
-	"description": "Golden Elephant Pendant Chain Necklace",
-	"collection": "best_seller",
-	"product_type": "jewellery",
-	"price": "65.00"
-	},
-	{
-	"product_id": "0_152",
-	"description": "Yellow gold Elephant Pendant Chain Necklace",
-	"collection":  "limited_edition",
-	"product_type": "jewellery",
-	"price": "75.00"	
-	},
-	{
-	"product_id": "0_153",
-	"description": "Yellow gold with dimonds Elephant Pendant Chain Necklace",
-	"collection": "limited_edition",
-	"product_type": "jewellery",
-	"price": "89.00"	
-	},
-	{
-	"product_id": "0_154",
-	"description": "Crystal 14k Rose Gold Over Silver Plated Elephant Pendant Necklace",
-	"collection": "new_arrivals",
-	"product_type": "jewellery",
-	"price": "95.00"	
-	},
-	{
-	"product_id": "0_155",
-	"description": " Diamond and 10k Gold Elephant Pendant Necklace",
-	"collection": "new_arrivals",
-	"product_type": "jewellery",
-	"price": "74.00"	
-	},
-	{
-	"product_id": "0_156",
-	"description": "Diamond Accent Elephant Pendant Chain",
-	"collection": "sale",
-	"product_type": "jewellery",
-	"price": "58.00"	
-	},
-	{
-	"product_id": "0_157",
-	"description": "Sterling Silver Elephant Pendant Necklace",
-	"collection": "sale",
-	"product_type": "jewellery",
-	"price": "43.00"	
-	},
-	{
-	"product_id": "0_158",
-	"description": "Sterling Silver Elephant Pendant Necklace",
-	"collection": "best_seller",
-	"product_type": "jewellery",
-	"price": "43.00"
-	},
-	{
-	"product_id": "0_159",
-	"description": "Golden and dimonds Elephant Pendant Necklace",
-	"collection": "best_seller",
-	"product_type": "jewellery",
-	"price": "43.00"
-	},
-	{
-	"product_id": "0_159",
-	"description": "Red Gold and dimonds Elephant Pendant Necklace",
-	"collection": "best_seller",
-	"product_type": "jewellery",
-	"price": "83.00"
-	},
-	{
-	"product_id": "1_150",
-	"description": "Handmade pendant made of glass on cabachon base",
-	"collection": "best_seller",
-	"product_type": "pendant",
-	"price": "12.00"	
-	},
-	{
-	"product_id": "1_151",
-	"description": "Handmade beautifully handcrafted made of glass on cabachon base",
-	"collection": "limited_edition",
-	"product_type": "pendant",
-	"price": "10.00"	
-	},
-	{
-	"product_id": "1_152",
-	"description": "Handmade beautifully handcrafted made of glass on cabachon base",
-	"collection": "new_arrivals",
-	"product_type": "pendant",
-	"price": "10.00"
-	},
-	{
-	"product_id": "1_153",
-	"description": "Handmade pendant made of glass on cabachon base",
-	"collection": "sale",
-	"product_type": "pendant",
-	"price": "10.00"
-	},
-	{
-	"product_id": "1_154",
-	"description": "Handmade pendant made of glass on cabachon base",
-	"collection": "sale",
-	"product_type": "pendant",
-	"price": "12.00"	
-	},
-	{
-	"product_id": "1_155",
-	"description": "Handmade pendant made of glass on cabachon base",
-	"collection": "sale",
-	"product_type": "pendant",
-	"price": "18.00"
-	},
-	{
-	"product_id": "1_156",
-	"description": "Handmade Mother & Baby Sterling Silver Elephant Necklace",
-	"collection": "limited_edition",
-	"product_type": "pendant",
-	"price": "54.00"
-	},
-	{
-	"product_id": "2_150",
-	"description": "T-shirt, size: M",
-	"collection": "new_arrivals",
-	"product_type": "t-shirt",
-	"price": "22.00"	
-	},
-	{
-	"product_id": "2_151",
-	"description": "T-shirt, size: S",
-	"collection": "sale",
-	"product_type": "t-shirt",
-	"price": "22.00"	
-	},
-	{
-	"product_id": "2_152",
-	"description": "T-shirt, size: L",
-	"collection": "limited_edition",
-	"product_type": "t-shirt",
-	"price": "22.00"	
-	},
-	{
-	"product_id": "2_153",
-	"description": "T-shirt, size: S",
-	"collection": "sale",
-	"product_type": "t-shirt",
-	"price": "22.00"
-	},
-	{
-	"product_id": "2_154",
-	"description": "T-shirt, size: M",
-	"collection": "new_arrivals",
-	"product_type": "t-shirt",
-	"price": "22.00"	
-	},
-	{
-	"product_id": "2_155",
-	"description": "T-shirt, size: M",
-	"collection": "new_arrivals",
-	"product_type": "t-shirt",
-	"price": "22.00"	
-	},
-	{
-	"product_id": "2_156",
-	"description": "T-shirt, size: S",
-	"collection": "sale",
-	"product_type": "t-shirt",
-	"price": "22.00"	
-	},
-	{
-	"product_id": "2_157",
-	"description": "T-shirt, size: L",
-	"collection": "limited_edition",
-	"product_type": "t-shirt",
-	"price": "22.00"	
-	},
-	{
-	"product_id": "2_158",
-	"description": "T-shirt, size: L",
-	"collection": "best_seller",
-	"product_type": "t-shirt",
-	"price": "22.00"	
-	},
-	{
-	"product_id": "2_159",
-	"description": "T-shirt, size: M",
-	"collection": "best_seller",
-	"product_type": "t-shirt",
-	"price": "22.00"	
-	},
-	{
-	"product_id": "2_160",
-	"description": "T-shirt, size: S",
-	"collection": "new_arrivals",
-	"product_type": "t-shirt",
-	"price": "22.00"	
-	},
-	{
-	"product_id": "3_151",
-	"description": "Mug with elephant",
-	"collection": "limited_edition",
-	"product_type": "mug",
-	"price": "12.00"
-	},
-	{
-	"product_id": "3_152",
-	"description": "Mug with elephant",
-	"collection": "new_arrivals",
-	"product_type": "mug",
-	"price": "12.00"	
-	},
-	{
-	"product_id": "3_153",
-	"description": "Mug with elephant",
-	"collection": "best_seller",
-	"product_type": "mug",
-	"price": "12.00"	
-	},
-	{
-	"product_id": "3_154",
-	"description": "Mug with elephant",
-	"collection": "new_arrivals",
-	"product_type": "mug",
-	"price": "12.00"
-	},
-	{
-	"product_id": "3_155",
-	"description": "Mug with elephant",
-	"collection": "sale",
-	"product_type": "mug",
-	"price": "12.00"
-	},
-	{
-	"product_id": "3_156",
-	"description": "Mug with elephant",
-	"collection": "limited_edition",
-	"product_type": "mug",
-	"price": "12.00"
-	},
-	{
-	"product_id": "4_150",
-	"description": "Elephant Decor",
-	"collection": "new_arrivals",
-	"product_type": "poster",
-	"price": "17.00"	
-	},
-	{
-	"product_id": "4_151",
-	"description": "Elephant Decor",
-	"collection": "new_arrivals",
-	"product_type": "poster",
-	"price": "17.00"	
-	},
-	{
-	"product_id": "4_152",
-	"description": "Elephant Decor",
-	"collection": "limited_edition",
-	"product_type": "poster",
-	"price": "17.00"	
-	},
-	{
-	"product_id": "4_153",
-	"description": "Elephant Decor",
-	"collection": "best_seller",
-	"product_type": "poster",
-	"price": "17.00"	
-	},
-	{
-	"product_id": "4_154",
-	"description": "Elephant Decor",
-	"collection": "best_seller",
-	"product_type": "poster",
-	"price": "17.00"	
-	},
-	{
-	"product_id": "4_155",
-	"description": "Elephant Decor",
-	"collection": "sale",
-	"product_type": "poster",
-	"price": "17.00"	
-	},
-	{
-	"product_id": "5_150",
-	"description": "Elephant Cushion",
-	"collection": "new_arrivals",
-	"product_type": "cushion",
-	"price": "19.00"	
-	},
-	{
-	"product_id": "5_151",
-	"description": "Elephant Cushion",
-	"collection": "new_arrivals",
-	"product_type": "cushion",
-	"price": "19.00"
-	},
-	{
-	"product_id": "5_152",
-	"description": "Elephant Cushion",
-	"collection": "new_arrivals",
-	"product_type": "cushion",
-	"price": "19.00"	
-	},
-	{
-	"product_id": "5_153",
-	"description": "Elephant Cushion",
-	"collection": "sale",
-	"product_type": "cushion",
-	"price": "19.00"	
-	},
-	{
-	"product_id": "5_154",
-	"description": "Elephant Cushion",
-	"collection": "sale",
-	"product_type": "cushion",
-	"price": "19.00"	
-	},
-	{
-	"product_id": "5_155",
-	"description": "Elephant Cushion",
-	"collection": "best_seller",
-	"product_type": "cushion",
-	"price": "19.00"	
-	},
-	{
-	"product_id": "5_156",
-	"description": "Elephant Cushion",
-	"collection": "best_seller",
-	"product_type": "cushion",
-	"price": "19.00"
-	},
-	{
-	"product_id": "5_157",
-	"description": "Elephant Cushion",
-	"collection": "limited_edition",
-	"product_type": "cushion",
-	"price": "19.00"
-	},
-	{
-	"product_id": "5_158",
-	"description": "Elephant Cushion",
-	"collection": "limited_edition",
-	"product_type": "cushion",
-	"price": "19.00"	
-	},
-	{
-	"product_id": "6_150",
-	"description": "Elephant Art Calendar 2017",
-	"collection": "sale",
-	"product_type": "calendar",
-	"price": "7.00"	
-	},
-	{
-	"product_id": "6_151",
-	"description": "Elephant Art Calendar 2017",
-	"collection": "sale",
-	"product_type": "calendar",
-	"price": "7.00"	
-	},
-	{
-	"product_id": "7_150",
-	"description": "Elephant Art",
-	"collection": "best_seller",
-	"product_type": "drawing",
-	"price": "21.00"
-	},
-	{
-	"product_id": "7_151",
-	"description": "Elephant Art",
-	"collection": "best_seller",
-	"product_type": "drawing",
-	"price": "21.00"	
-	},
-	{
-	"product_id": "7_152",
-	"description": "Elephant Art",
-	"collection": "best_seller",
-	"product_type": "drawing",
-	"price": "21.00"
-	},
-	{
-	"product_id": "7_153",
-	"description": "Elephant Art",
-	"collection": "new_arrivals",
-	"product_type": "drawing",
-	"price": "27.00"	
-	},
-	{
-	"product_id": "7_154",
-	"description": "Elephant Art",
-	"collection": "new_arrivals",
-	"product_type": "drawing",
-	"price": "35.00"	
-	},
-	{
-	"product_id": "7_155"
-	"description": "Elephant Art"
-	"collection": "limited_edition",
-	"product_type": "drawing",
-	"price": "54.00"	
-	},
-	{
-	"product_id": "7_156",
-	"description": "Elephant Art",
-	"collection": "sale",
-	"product_type": "drawing",
-	"price": "41.00"	
-	}
-	]
+"product_id": "0_150",
+"description": "Silver Elephant Pendant Chain Necklace",
+"collection": "best_seller",
+"product_type": "jewellery",
+"price": "30.00"
+},
+{
+"product_id": "0_151",
+"description": "Golden Elephant Pendant Chain Necklace",
+"collection": "best_seller",
+"product_type": "jewellery",
+"price": "65.00"
+},
+{
+"product_id": "0_152",
+"description": "Yellow gold Elephant Pendant Chain Necklace",
+"collection":  "limited_edition",
+"product_type": "jewellery", 
+"price": "75.00"	
+},
+{
+"product_id": "0_153",
+"description": "Yellow gold with dimonds Elephant Pendant Chain Necklace",
+"collection": "limited_edition",
+"product_type": "jewellery",
+"price": "89.00"	
+},
+{
+"product_id": "0_154",
+"description": "Crystal 14k Rose Gold Over Silver Plated Elephant Pendant Necklace",
+"collection": "new_arrivals",
+"product_type": "jewellery",
+"price": "95.00"	
+},
+{
+"product_id": "0_155",
+"description": " Diamond and 10k Gold Elephant Pendant Necklace",
+"collection": "new_arrivals",
+"product_type": "jewellery",
+"price": "74.00"	
+},
+{
+"product_id": "0_156",
+"description": "Diamond Accent Elephant Pendant Chain",
+"collection": "sale",
+"product_type": "jewellery",
+"price": "58.00"	
+},
+{
+"product_id": "0_157",
+"description": "Sterling Silver Elephant Pendant Necklace",
+"collection": "sale",
+"product_type": "jewellery",
+"price": "43.00"	
+},
+{
+"product_id": "0_158",
+"description": "Sterling Silver Elephant Pendant Necklace",
+"collection": "best_seller",
+"product_type": "jewellery",
+"price": "43.00"
+},
+{
+"product_id": "0_159",
+"description": "Golden and dimonds Elephant Pendant Necklace",
+"collection": "best_seller",
+"product_type": "jewellery",
+"price": "43.00"
+},
+{
+"product_id": "0_159",
+"description": "Red Gold and dimonds Elephant Pendant Necklace",
+"collection": "best_seller",
+"product_type": "jewellery",
+"price": "83.00"
+},
+{
+"product_id": "1_150",
+"description": "Handmade pendant made of glass on cabachon base",
+"collection": "best_seller",
+"product_type": "pendant",
+"price": "12.00"	
+},
+{
+"product_id": "1_151",
+"description": "Handmade beautifully handcrafted made of glass on cabachon base",
+"collection": "limited_edition",
+"product_type": "pendant",
+"price": "10.00"	
+},
+{
+"product_id": "1_152",
+"description": "Handmade beautifully handcrafted made of glass on cabachon base",
+"collection": "new_arrivals",
+"product_type": "pendant",
+"price": "10.00"
+},
+{
+"product_id": "1_153",
+"description": "Handmade pendant made of glass on cabachon base",
+"collection": "sale",
+"product_type": "pendant",
+"price": "10.00"
+},
+{
+"product_id": "1_154",
+"description": "Handmade pendant made of glass on cabachon base",
+"collection": "sale",
+"product_type": "pendant",
+"price": "12.00"	
+},
+{
+"product_id": "1_155",
+"description": "Handmade pendant made of glass on cabachon base",
+"collection": "sale",
+"product_type": "pendant",
+"price": "18.00"
+},
+{
+"product_id": "1_156",
+"description": "Handmade Mother & Baby Sterling Silver Elephant Necklace",
+"collection": "limited_edition",
+"product_type": "pendant",
+"price": "54.00"
+},
+{
+"product_id": "2_150",
+"description": "T-shirt, size: M",
+"collection": "new_arrivals",
+"product_type": "t-shirt",
+"price": "22.00"	
+},
+{
+"product_id": "2_151",
+"description": "T-shirt, size: S",
+"collection": "sale",
+"product_type": "t-shirt",
+"price": "22.00"	
+},
+{
+"product_id": "2_152",
+"description": "T-shirt, size: L",
+"collection": "limited_edition",
+"product_type": "t-shirt",
+"price": "22.00"	
+},
+{
+"product_id": "2_153",
+"description": "T-shirt, size: S",
+"collection": "sale",
+"product_type": "t-shirt",
+"price": "22.00"
+},
+{
+"product_id": "2_154",
+"description": "T-shirt, size: M",
+"collection": "new_arrivals",
+"product_type": "t-shirt",
+"price": "22.00"	
+},
+{
+"product_id": "2_155",
+"description": "T-shirt, size: M",
+"collection": "new_arrivals",
+"product_type": "t-shirt",
+"price": "22.00"	
+},
+{
+"product_id": "2_156",
+"description": "T-shirt, size: S",
+"collection": "sale",
+"product_type": "t-shirt",
+"price": "22.00"	
+},
+{
+"product_id": "2_157",
+"description": "T-shirt, size: L",
+"collection": "limited_edition",
+"product_type": "t-shirt",
+"price": "22.00"	
+},
+{
+"product_id": "2_158",
+"description": "T-shirt, size: L",
+"collection": "best_seller",
+"product_type": "t-shirt",
+"price": "22.00"	
+},
+{
+"product_id": "2_159",
+"description": "T-shirt, size: M",
+"collection": "best_seller",
+"product_type": "t-shirt",
+"price": "22.00"	
+},
+{
+"product_id": "2_160",
+"description": "T-shirt, size: S",
+"collection": "new_arrivals",
+"product_type": "t-shirt",
+"price": "22.00"	
+},
+{
+"product_id": "3_151",
+"description": "Mug with elephant",
+"collection": "limited_edition",
+"product_type": "mug",
+"price": "12.00"
+},
+{
+"product_id": "3_152",
+"description": "Mug with elephant",
+"collection": "new_arrivals",
+"product_type": "mug",
+"price": "12.00"	
+},
+{
+"product_id": "3_153",
+"description": "Mug with elephant",
+"collection": "best_seller",
+"product_type": "mug",
+"price": "12.00"	
+},
+{
+"product_id": "3_154",
+"description": "Mug with elephant",
+"collection": "new_arrivals",
+"product_type": "mug",
+"price": "12.00"
+},
+{
+"product_id": "3_155",
+"description": "Mug with elephant",
+"collection": "sale",
+"product_type": "mug",
+"price": "12.00"
+},
+{
+"product_id": "3_156",
+"description": "Mug with elephant",
+"collection": "limited_edition",
+"product_type": "mug",
+"price": "12.00"
+},
+{
+"product_id": "4_150",
+"description": "Elephant Decor",
+"collection": "new_arrivals",
+"product_type": "poster",
+"price": "17.00"	
+},
+{
+"product_id": "4_151",
+"description": "Elephant Decor",
+"collection": "new_arrivals",
+"product_type": "poster",
+"price": "17.00"	
+},
+{
+"product_id": "4_152",
+"description": "Elephant Decor",
+"collection": "limited_edition",
+"product_type": "poster",
+"price": "17.00"	
+},
+{
+"product_id": "4_153",
+"description": "Elephant Decor",
+"collection": "best_seller",
+"product_type": "poster",
+"price": "17.00"	
+},
+{
+"product_id": "4_154",
+"description": "Elephant Decor",
+"collection": "best_seller",
+"product_type": "poster",
+"price": "17.00"	
+},
+{
+"product_id": "4_155",
+"description": "Elephant Decor",
+"collection": "sale",
+"product_type": "poster",
+"price": "17.00"	
+},
+{
+"product_id": "5_150",
+"description": "Elephant Cushion",
+"collection": "new_arrivals",
+"product_type": "cushion",
+"price": "19.00"	
+},
+{
+"product_id": "5_151",
+"description": "Elephant Cushion",
+"collection": "new_arrivals",
+"product_type": "cushion",
+"price": "19.00"
+},
+{
+"product_id": "5_152",
+"description": "Elephant Cushion",
+"collection": "new_arrivals",
+"product_type": "cushion",
+"price": "19.00"	
+},
+{
+"product_id": "5_153",
+"description": "Elephant Cushion",
+"collection": "sale",
+"product_type": "cushion",
+"price": "19.00"	
+},
+{
+"product_id": "5_154",
+"description": "Elephant Cushion",
+"collection": "sale",
+"product_type": "cushion",
+"price": "19.00"	
+},
+{
+"product_id": "5_155",
+"description": "Elephant Cushion",
+"collection": "best_seller",
+"product_type": "cushion",
+"price": "19.00"	
+},
+{
+"product_id": "5_156",
+"description": "Elephant Cushion",
+"collection": "best_seller",
+"product_type": "cushion",
+"price": "19.00"
+},
+{
+"product_id": "5_157",
+"description": "Elephant Cushion",
+"collection": "limited_edition",
+"product_type": "cushion",
+"price": "19.00"
+},
+{
+"product_id": "5_158",
+"description": "Elephant Cushion",
+"collection": "limited_edition",
+"product_type": "cushion",
+"price": "19.00"	
+},
+{
+"product_id": "6_150",
+"description": "Elephant Art Calendar 2017",
+"collection": "sale",
+"product_type": "calendar",
+"price": "7.00"	
+},
+{
+"product_id": "6_151",
+"description": "Elephant Art Calendar 2017",
+"collection": "sale",
+"product_type": "calendar",
+"price": "7.00"	
+},
+{
+"product_id": "7_150",
+"description": "Elephant Art",
+"collection": "best_seller",
+"product_type": "drawing",
+"price": "21.00"
+},
+{
+"product_id": "7_151",
+"description": "Elephant Art",
+"collection": "best_seller",
+"product_type": "drawing",
+"price": "21.00"	
+},
+{
+"product_id": "7_152",
+"description": "Elephant Art",
+"collection": "best_seller",
+"product_type": "drawing",
+"price": "21.00"
+},
+{
+"product_id": "7_153",
+"description": "Elephant Art",
+"collection": "new_arrivals",
+"product_type": "drawing",
+"price": "27.00"	
+},
+{
+"product_id": "7_154",
+"description": "Elephant Art",
+"collection": "new_arrivals",
+"product_type": "drawing",
+"price": "35.00"	
+},
+{
+"product_id": "7_155"
+"description": "Elephant Art"
+"collection": "limited_edition",
+"product_type": "drawing",
+"price": "54.00"	
+},
+{
+"product_id": "7_156",
+"description": "Elephant Art",
+"collection": "sale",
+"product_type": "drawing",
+"price": "41.00"	
 }
+]
+
 
 
 
